4ad8fd1016a06e5dcff32a53ace04ffda17f45a6
anovic123 <105565287+anovic123@users.noreply.github.com>
2022-10-29 10:04:54 +0300

    001

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ function App() {
         <Header onClickCart={() => setCartOpened(true)} />
         <Routes>
           <Route
-            path=""
+            path="react-sneakers"
             element={
               <Home
                 items={items}
@@ -138,8 +138,8 @@ function App() {
               />
             }
           />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="orders" element={<Orders />} />
+          <Route path="react-sneakers/favorites" element={<Favorites />} />
+          <Route path="react-sneakers/orders" element={<Orders />} />
         </Routes>
       </div>
     </AppContext.Provider>
